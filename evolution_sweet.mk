7bebce98bdbefb4910f67175bdf7d154cadd4391
WillyAbel <monterowilly1@gmail.com>
2023-10-09 15:31:24 -0400

    sweet: Evolution X Build Flags

--- evolution_sweet.mk
+++ evolution_sweet.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common evolution stuff
-TARGET_SUPPORTS_QUICK_TAP := true
+
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
@@ -26,4 +26,16 @@ PRODUCT_MANUFACTURER := Xiaomi
     <string name="build_maintainer_donate_url">https://t.me/updatewillyAbel</string>
       <string name="build_status_summary">UNOFFICIAL</string>
 
+#GAPPS
+TARGET_USES_MINI_GAPPS := false
+TARGET_USES_PICO_GAPPS := false
+
+#EvoX Stuffs
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_USE_PIXEL_LAUNCHER := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := false
+TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
